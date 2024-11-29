a75bbc9983f325084a1fe0dcb8a6587e52741a6d
Deston Bui <khangduy158@gmail.com>
2024-11-29 17:25:51 -0500

    TASK 2: API book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,7 +18,14 @@ public_users.get("/", function (req, res) {
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  const result = books[isbn];
+
+  if (result) {
+    return res.status(200).send(JSON.stringify(result, null, 4));
+  } else {
+    return res.status(204).json({ message: "Book not found!" });
+  }
 });
 
 // Get book details based on author
