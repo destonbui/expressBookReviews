2295d512ed8000306ebe4f1012fce5b711ab62fc
Deston Bui <khangduy158@gmail.com>
2024-11-29 21:37:06 -0500

    Task 8: API put - Authenticated user add review to book by ISBN

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,49 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const { users } = require("./router/auth_users.js");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  //Write the authenication mechanism here
+  const authHeader = req.headers["authorization"];
+
+  //check if there's auth header
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    const token = authHeader.split(" ")[1];
+
+    const { username } = jwt.verify(token, process.env.SECRET);
+    const isValid = users.find((user) => user.username === username);
+
+    if (isValid) {
+      //user authorized
+      next();
+    } else {
+      return res
+        .status(401)
+        .json({ message: "You are not authorized. Please sign in." });
+    }
+  }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -44,13 +44,41 @@ regd_users.post("/login", (req, res) => {
   const token = jwt.sign({ username: user.username }, process.env.SECRET, {
     expiresIn: "1h",
   });
-  return res.status(200).json({ message: "Customer successfully signed in." });
+  return res
+    .status(200)
+    .json({ message: "Customer successfully signed in.", token });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  const { review } = req.body;
+  const authHeader = req.header("authorization");
+  let usernameFromToken;
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    const token = authHeader.split(" ")[1];
+
+    const { username } = jwt.verify(token, process.env.SECRET);
+    usernameFromToken = username;
+  }
+
+  //Check if isbn has result in database
+  const result = books[isbn];
+
+  if (!result) {
+    return res.status(204).json({ message: "Book not found!" });
+  }
+
+  //check if there is review
+  if (!review || review === "") {
+    return res.status(204).json({ message: "Missing review." });
+  }
+
+  books[isbn].reviews[usernameFromToken] = {
+    review: review,
+  };
+  return res.status(200).send(JSON.stringify(books[isbn], null, 4));
 });
 
 module.exports.authenticated = regd_users;
